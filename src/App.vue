7260d5b834924a93d99dbbfa7e2858bfa6d0a0ca
MohamedBehhar <bahharmohamed18@gmail.com>
2023-05-24 17:10:51 +0100

    technical test

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script >
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import user from "./assets/user.json"
+
+export default {
+  data(){
+    return {
+      users: user
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <div  v-for="(user, id) in users" :key="id">
+      <h1>{{ user.name }}</h1>
+      <p>Age: {{  user.age}}</p>
+    </div>
+  </div>
+
+</template>
+
+<style scoped>
+
+
+</style>
